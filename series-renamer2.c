5d283513ae302a991fa957469b0e3423937ca046
Scott McCaffrey <mccaffrey.scott@gmail.com>
2022-05-17 21:10:54 -0600

    version used to remove seasons with downloaded text

--- series-renamer2.c
+++ series-renamer2.c
@@ -25,12 +25,57 @@ int find_special_char(char * dir_name) {
     return 0;
 }
 
-void scan_series_dirs(char * path){
+
+#define SEASON_ATOI_BUF_SIZE 3
+#define EAT_FORWARD 3
+int determine_season(char * filename) {
+
+    const char* const season_str = "Season";
+    char season[SEASON_ATOI_BUF_SIZE];
+    int i = 0;
+    char *pch = NULL;
+    int found_digit = 0;
+    int season_num = 0;
+
+    pch = strstr(filename, season_str);
+    if (pch) {
+        //printf("    * pch = %s   ", pch);
+        pch += (strlen(season_str));
+
+        while (i < EAT_FORWARD) {
+            if (isdigit(pch[0])) {
+                found_digit = 1;
+                break;
+            }
+            pch += 1;
+            i++;
+        }
+
+        if (found_digit) {
+            i = 0;
+            while (i < SEASON_ATOI_BUF_SIZE) {
+                if (isdigit(pch[0])) {
+                    season[i] = pch[0];
+                    pch += 1;
+                    i++;
+                } else {
+                    break;
+                }
+            }
+            season_num = atoi(season);
+        }
+    }
+
+    return season_num;
+}
+
+void scan_series_dirs(char * path, char * series_name){
 
     DIR *d;
     struct dirent *dir;
     int skip = 2;
     char buf[MAX_FILENAME];
+    int season = 0;
 
     d = opendir(path);
     if (d) {
@@ -38,15 +83,24 @@ void scan_series_dirs(char * path){
             if (skip-- > 0)
                 continue;
 
-            // if not a folder
             sprintf(buf, "%s\\%s", path, dir->d_name);
             if (is_regular_file(buf)) {
                 if (is_movie_file(dir->d_name)) {
                     //printf(" * Movie: %s\n", dir->d_name);
                 } else {
                    // printf(" - RANDOM: %s\n", dir->d_name);
-                   printf("rm \"%s\"\n", buf); 
+                   //printf("rm \"%s\"\n", buf); 
                 }
+            } else {
+                season = 0;
+                if (find_special_char(dir->d_name)) {
+                    season = determine_season(dir->d_name);
+                    //printf("    * RENAME: %s\n", dir->d_name);
+                    printf("mv \"%s\\%s\" \"%s\\%s - Season %d\n", series_name, dir->d_name, series_name, series_name, season);
+                }
+                
+                
+                //printf("    * %d - %s\n", season, dir->d_name);
             }
 
         }
@@ -73,7 +127,7 @@ void scan_base_dir(char * path) {
             
             sprintf(buf, "%s\\%s", path, dir->d_name);
             //printf("%s\n", buf);
-            scan_series_dirs(buf);
+            scan_series_dirs(buf, dir->d_name);
             //printf("\n");
 
         }
@@ -91,8 +145,8 @@ int main( int argc, char *argv[] )
     char tmp_dir[MAX_PATH];
 
     
-    //scan_base_dir(s_rouse_dir);
-    scan_base_dir(big_olive_dir);
+    scan_base_dir(s_rouse_dir);
+    //scan_base_dir(big_olive_dir);
     
     
     
